6858f06b952e776b04d51ee8b5c89566aca8910d
Marcelo <marcelothebuilder@users.noreply.github.com>
2023-02-18 14:59:48 -0300

    fixes package.json

--- src/inject.js
+++ src/inject.js
@@ -1,34 +1,33 @@
-const DarkReader = require("darkreader");
+/* eslint-disable no-undef */
+const DarkReader = require('darkreader');
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  let isDarkReaderEnabled = false;
 
-    let isDarkReaderEnabled = false;
+  try {
+    console.log('DomContentLoaded');
 
-    try {
-        console.log('DomContentLoaded');
+    setInterval(() => {
+      const { hostname } = document.location;
+      if (hostname === 'accounts.google.com') {
+        console.log('DisablingDarkReader');
+        if (!isDarkReaderEnabled) return;
+        isDarkReaderEnabled = false;
+        DarkReader.disable();
+      } else {
+        if (isDarkReaderEnabled) return;
+        console.log('EnablingDarkReader');
+        isDarkReaderEnabled = true;
+        DarkReader.auto({
+          brightness: 100,
+          contrast: 90,
+          sepia: 10,
+        });
+      }
+    }, 100);
 
-        setInterval(() => {
-            const { hostname } = document.location;
-            if (hostname === 'accounts.google.com') {
-                console.log('DisablingDarkReader');
-                if (!isDarkReaderEnabled) return;
-                isDarkReaderEnabled = false;
-                DarkReader.disable();
-            } else {
-                if (isDarkReaderEnabled) return;
-                console.log('EnablingDarkReader');
-                isDarkReaderEnabled = true;
-                DarkReader.auto({
-                    brightness: 100,
-                    contrast: 90,
-                    sepia: 10
-                });
-            }
-        }, 100);
-
-        console.log('DarkReader initialized');
-    } catch(e) {
-        console.log('error', e);
-    };
+    console.log('DarkReader initialized');
+  } catch (e) {
+    console.log('error', e);
+  }
 });
-
